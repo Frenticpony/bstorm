2f4a968d5160096623e05a431ca965c140238dc3
neriplum <neriplum@gmail.com>
2018-01-04 14:53:20 +0900

    remove debug output

--- bsengine/src/script.cpp
+++ bsengine/src/script.cpp
@@ -82,8 +82,6 @@ namespace bstorm {
       srcMap = codeGen.getSourceMap();
       engine->logInfo("codegen completed.");
 
-      engine->logInfo(codeGen.getCode());
-
       // ランタイム読み込み
       luaL_loadbuffer(L, (const char *)luaJIT_BC_runtime, luaJIT_BC_runtime_SIZE, DNH_RUNTIME_NAME);
       callLuaChunk();
