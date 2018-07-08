4ae960f6f7cb08e53bb34f309a151390d0743da7
neriplum <neriplum@gmail.com>
2018-07-09 00:03:42 +0900

    バイトコード生成用関数

--- bsengine/src/bstorm/lua_util.cpp
+++ bsengine/src/bstorm/lua_util.cpp
@@ -1,5 +1,17 @@
 #include <bstorm/lua_util.hpp>
 
+static int AccumChunkSize(lua_State* L, const void* data, size_t dataSize, size_t* totalSize)
+{
+    *totalSize += dataSize;
+    return 0;
+}
+
+static int ChunkWriter(lua_State* L, const void* data, size_t dataSize, std::string* dst)
+{
+    dst->append((const char*)data, dataSize);
+    return 0;
+}
+
 namespace bstorm
 {
 void* GetPointerFromLuaRegistry(lua_State* L, const char* key)
@@ -17,4 +29,13 @@ void SetPointerToLuaRegistry(lua_State* L, const char* key, void* p)
     lua_rawset(L, LUA_REGISTRYINDEX);
 }
 
+void SerializeChunk(lua_State * L, std::string & byteCode)
+{
+    // 最初に全チャンク片の大きさの合計を求めてメモリを確保
+    size_t totalSize = 0;
+    lua_dump(L, (lua_Writer)AccumChunkSize, &totalSize);
+    byteCode.clear();
+    byteCode.reserve(totalSize);
+    lua_dump(L, (lua_Writer)ChunkWriter, &byteCode);
+}
 }
--- bsengine/src/bstorm/lua_util.hpp
+++ bsengine/src/bstorm/lua_util.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <string>
 #include <luajit/lua.hpp>
 
 namespace bstorm
 {
 void* GetPointerFromLuaRegistry(lua_State* L, const char* key);
 void SetPointerToLuaRegistry(lua_State* L, const char* key, void* p);
+
+// serialize stack top chunk. this function doesn't remove stack top.
+void SerializeChunk(lua_State* L, std::string& byteCode);
 }
