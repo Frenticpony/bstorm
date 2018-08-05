8a49c8e37a3eeea59a85f499407473aa00ff59bc
neriplum <neriplum@gmail.com>
2018-08-06 00:09:11 +0900

    into namespace

--- bsengine/src/bstorm/path_const.hpp
+++ bsengine/src/bstorm/path_const.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+namespace bstorm
+{
 constexpr wchar_t* FREE_PLAYER_DIR = L"script/player";
 
 constexpr wchar_t* DEFAULT_SYSTEM_PATH = L"script/default_system/Default_System.txt";
@@ -10,3 +12,4 @@ constexpr wchar_t* DEFAULT_PACKAGE_ARGS_COMMON_DATA_AREA_NAME = L"__DEFAULT_PACK
 constexpr wchar_t* SYSTEM_STG_DIGIT_IMG_PATH = L"resource/img/System_Stg_Digit.png";
 constexpr wchar_t* SYSTEM_SINGLE_STAGE_PATH = L"resource/script/System_SingleStage.txt";
 constexpr wchar_t* SYSTEM_PLURAL_STAGE_PATH = L"resource/script/System_PluralStage.txt";
+}
